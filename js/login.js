7f755d88d61ba4cf7f1c601ce1a2badf2b0c9628
JeffMota <jmota906@gmail.com>
2022-10-27 11:27:07 -0300

    Adição da tela de login

---
+++ js/login.js
@@ -0,0 +1,28 @@
+const input_name = document.querySelector('.input_name');
+const btn_entr = document.querySelector('.btn_entrar');
+const img_loading = document.querySelector('.loading');
+
+var usuario = { name: '' };
+
+function entrar() {
+    input_name.classList.toggle('hidden');
+    img_loading.classList.toggle('hidden');
+
+    if (input_name.value == "") {
+        alert('Digite o seu nome para seus amigos te reconhecerem!');
+    }
+    else {
+        usuario.name = input_name.value;
+
+        const promise = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants', usuario);
+        promise.then(() => { window.location.href = 'index.html' });
+        promise.catch(() => {
+            alert('Nome de usuário já cadastrado');
+            input_name.classList.toggle('hidden');
+            img_loading.classList.toggle('hidden');
+        });
+    }
+}
+
+btn_entr.addEventListener('click', entrar);
+
