83c7be648a88be3059cb35089c1813fa6f40e97e
JeffMota <jmota906@gmail.com>
2022-10-28 13:03:06 -0300

    envio de mensagem reservadamente

--- js/script.js
+++ js/script.js
@@ -3,10 +3,14 @@ const btn_menu = document.querySelector('.menu');
 const chat = document.querySelector('.chat');
 const campo = document.querySelector('.campo');
 const btn_enviar = document.querySelector('.btn_enviar');
+const ativos = document.querySelector('.ativos');
 var mensagens;
 
 var ultimaMensagem;
 var penultimaMensagem;
+var numeroParticipantes = 0;
+var destinatario = 'Todos';
+var tipo = 'message';
 
 var usuario = {
     name: ''
@@ -15,34 +19,89 @@ usuario.name = localStorage.getItem('user');
 localStorage.removeItem('user');
 
 var mensagem = {
-	from: "",
-	to: "Todos",
-	text: "",
-	type: "message"
+    from: "",
+    to: "Todos",
+    text: "",
+    type: "message"
 }
 
-function enviarMensagem(){
+function enviarMensagem() {
     const promessa = axios.post('https://mock-api.driven.com.br/api/v6/uol/messages', mensagem);
-    promessa.then(() => {buscarMSG()});
-    promessa.catch(() => {window.location.href = 'login.html'});
+    promessa.then(buscarMSG());
+    promessa.catch(() => { window.location.href = 'login.html' });
 }
-
 
 btn_enviar.addEventListener('click', () => {
-    if(campo.value != ''){
+    if (campo.value != '') {
         mensagem.from = usuario.name;
         mensagem.text = campo.value;
+        mensagem.to = destinatario;
+        mensagem.type = tipo;
         enviarMensagem();
     }
 
 })
 
+function escolher(elm) {
+    const part = document.querySelectorAll('.participante')
+    part.forEach(i => {
+        i.children[2].classList.add('escondido');
+    })
+    elm.children[2].classList.remove('escondido');
+    destinatario = elm.children[1].innerHTML;
+}
+
+function escolherTipo(elm){
+    const part = document.querySelectorAll('.tipo')
+    part.forEach(i => {
+        i.children[2].classList.add('escondido');
+    })
+    elm.children[2].classList.remove('escondido');
+    destinatario = elm.children[1].innerHTML;
+
+    if(elm.children[1].innerHTML == 'Reservadamente'){
+    tipo = 'private_message';
+    }
+    else {
+        tipo = 'message'
+    }
+}
+
+//Buscando Participantes
+function buscarParticipantes() {
+    const promise = axios.get('https://mock-api.driven.com.br/api/v6/uol/participants');
+    promise.then(response => {
+        const data = response.data;
+
+        if (data.length != numeroParticipantes) {
+            ativos.innerHTML = `
+            <div class="on participante" onclick="escolher(this)">
+                <img class="perfil" src="img/Vector.png" alt="todos">
+                <p>Todos</p>
+                <img class="escondido" src="img/check.png">
+            </div>
+            `;
+
+
+            data.forEach(elm => {
+                ativos.innerHTML += `
+                <div class="on participante" onclick="escolher(this)">
+                        <img class="perfil" src="img/perfil.png" alt="todos">
+                        <p>${elm.name}</p>
+                        <img class="escondido " src="img/check.png">
+                </div>
+            `
+            });
+            numeroParticipantes = data.length;
+        }
+    })
+}
 
 
 //Enviando status para permanecer online
 setInterval(() => {
     axios.post('https://mock-api.driven.com.br/api/v6/uol/status', usuario);
-    
+
 }, 5000);
 
 function fecharMenu() {
@@ -50,25 +109,25 @@ function fecharMenu() {
 }
 
 //Função que busca mensagens
-function renderizarMSGS(data){
+function renderizarMSGS(data) {
     chat.innerHTML = '';
 
     data.forEach(element => {
-        if(element.type == "status"){
+        if (element.type == "status") {
             chat.innerHTML += `
                 <section class="msg status" >
                     ${element.time} <span> ${element.from} </span> ${element.text}
                 </section>
             `
         }
-        else if(element.type == "message"){
+        else if (element.type == "message") {
             chat.innerHTML += `
                 <section class="msg" >
                     ${element.time} <span> ${element.from} </span> para <span>${element.to}</span> ${element.text}
                 </section>
             `
         }
-        else if(usuario.name == element.to){
+        else if (usuario.name == element.to || element.from == usuario.name) {
             chat.innerHTML += `
                 <section class="msg reservada" >
                     ${element.time} <span> ${element.from} </span> reservadamente para <span>${element.to}</span> ${element.text}
@@ -79,26 +138,28 @@ function renderizarMSGS(data){
 
     //Indicando ultima mensagem para scroll automatico
     mensagens = document.querySelectorAll('.msg');
-    if(mensagens[mensagens.length-2].classList.contains('ultima')){
-        mensagens[mensagens.length-2].classList.remove('ultima');
+    if (mensagens[mensagens.length - 2].classList.contains('ultima')) {
+        mensagens[mensagens.length - 2].classList.remove('ultima');
     }
-    mensagens[mensagens.length-1].classList.add('ultima');
+    mensagens[mensagens.length - 1].classList.add('ultima');
 
     ultimaMensagem = document.querySelector('.ultima');
 }
 
 //Buncando mensagens
-function buscarMSG(){
+function buscarMSG() {
     const promise = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages');
     promise.then(response => {
         const data = response.data;
 
         renderizarMSGS(data);
-        
+
         ultimaMensagem.scrollIntoView();
     });
 }
 
 buscarMSG();
+buscarParticipantes();
 setInterval(buscarMSG, 3000);
+setInterval(buscarParticipantes, 1000);
 
