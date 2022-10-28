69205b94f0c6ce380a7fb40cfb707f02510893aa
JeffMota <jmota906@gmail.com>
2022-10-28 10:34:57 -0300

    Scroll para ultima mensagem

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 const participantes = document.querySelector('.participantes');
 const btn_menu = document.querySelector('.menu');
 const chat = document.querySelector('.chat');
+var mensagens;
+
+var ultimaMensagem;
+var penultimaMensagem;
 
 var usuario = {
     name: ''
@@ -18,7 +22,10 @@ function fecharMenu() {
     participantes.classList.toggle('hidden');
 }
 
+//Função que busca mensagens
 function renderizarMSGS(data){
+    chat.innerHTML = '';
+
     data.forEach(element => {
         if(element.type == "status"){
             chat.innerHTML += `
@@ -34,18 +41,40 @@ function renderizarMSGS(data){
                 </section>
             `
         }
+        else if(usuario.name == element.to){
+            chat.innerHTML += `
+                <section class="msg reservada" >
+                    ${element.time} <span> ${element.from} </span> reservadamente para <span>${element.to}</span> ${element.text}
+                </section>
+            `
+        }
     });
+
+    //Indicando ultima mensagem para scroll automatico
+    mensagens = document.querySelectorAll('.msg');
+    if(mensagens[mensagens.length-2].classList.contains('ultima')){
+        mensagens[mensagens.length-2].classList.remove('ultima');
+    }
+    mensagens[mensagens.length-1].classList.add('ultima');
+
+    ultimaMensagem = document.querySelector('.ultima');
+    console.log(ultimaMensagem)
 }
 
+//Buncando mensagens
 function buscarMSG(){
     const promise = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages');
     promise.then(response => {
         const data = response.data;
-        console.log(data);
 
         renderizarMSGS(data);
+        
+        
+
+        ultimaMensagem.scrollIntoView();
     });
 }
 
 buscarMSG();
+setInterval(buscarMSG, 3000);
 
