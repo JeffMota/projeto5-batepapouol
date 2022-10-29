41ab23ab3a0ea78042b7c077918092d9c9a03db0
JeffMota <jmota906@gmail.com>
2022-10-29 09:51:28 -0300

    Correção de bug na escolha do destinatario

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ const campo = document.querySelector('.campo');
 const btn_enviar = document.querySelector('.btn_enviar');
 const ativos = document.querySelector('.ativos');
 const form = document.querySelector('.form');
+const dest = document.querySelector('.dest');
 var mensagens;
 
 var ultimaMensagem;
@@ -12,6 +13,9 @@ var penultimaMensagem;
 var numeroParticipantes = 0;
 var destinatario = 'Todos';
 var tipo = 'message';
+var destOn = false;
+
+var participanteOnline = [];
 
 var usuario = {
     name: ''
@@ -26,6 +30,7 @@ var mensagem = {
     type: "message"
 }
 
+//Eviar mensagem
 function enviarMensagem() {
     if (campo.value != '') {
         mensagem.from = usuario.name;
@@ -35,12 +40,13 @@ function enviarMensagem() {
     }
 
     const promessa = axios.post('https://mock-api.driven.com.br/api/v6/uol/messages', mensagem);
-    promessa.then(() => {buscarMSG()});
+    promessa.then(() => { buscarMSG() });
     promessa.catch(() => { window.location.href = 'index.html' });
 
     campo.value = '';
 }
 
+//Escolhendo detinatario
 function escolher(elm) {
     const part = document.querySelectorAll('.participante')
     part.forEach(i => {
@@ -48,21 +54,34 @@ function escolher(elm) {
     })
     elm.children[2].classList.remove('escondido');
     destinatario = elm.children[1].innerHTML;
+    if (destinatario === 'Todos') {
+        dest.classList.add('hidden');
+    }
+    else {
+        dest.classList.remove('hidden');
+    }
 }
 
-function escolherTipo(elm){
-    const part = document.querySelectorAll('.tipo')
+//Ecolhendo se é publica ou privada
+function escolherTipo(elm) {
+    const part = document.querySelectorAll('.tipo');
+
     part.forEach(i => {
         i.children[2].classList.add('escondido');
     })
     elm.children[2].classList.remove('escondido');
-    destinatario = elm.children[1].innerHTML;
 
-    if(elm.children[1].innerHTML == 'Reservadamente'){
-    tipo = 'private_message';
+    if (elm.children[1].innerHTML == 'Reservadamente') {
+        tipo = 'private_message';
+        dest.innerHTML = `
+        Enviando para ${destinatario} (reservadamente)
+    `
     }
     else {
         tipo = 'message'
+        dest.innerHTML = `
+            Enviando para ${destinatario}
+        `
     }
 }
 
@@ -71,26 +90,52 @@ function buscarParticipantes() {
     const promise = axios.get('https://mock-api.driven.com.br/api/v6/uol/participants');
     promise.then(response => {
         const data = response.data;
+        participanteOnline = [];
+        
+        
 
         if (data.length != numeroParticipantes) {
             ativos.innerHTML = `
             <div class="on participante" onclick="escolher(this)">
                 <img class="perfil" src="img/Vector.png" alt="todos">
                 <p>Todos</p>
-                <img class="" src="img/check.png">
+                <img class="escondido" src="img/check.png">
             </div>
             `;
 
 
             data.forEach(elm => {
-                ativos.innerHTML += `
-                <div class="on participante" onclick="escolher(this)">
-                        <img class="perfil" src="img/perfil.png" alt="todos">
-                        <p>${elm.name}</p>
-                        <img class="escondido " src="img/check.png">
-                </div>
-            `
+                if (elm.name != usuario.name) {
+                    ativos.innerHTML += `
+                    <div class="on participante" onclick="escolher(this)">
+                            <img class="perfil" src="img/perfil.png" alt="todos">
+                            <p>${elm.name}</p>
+                            <img class="escondido " src="img/check.png">
+                    </div>
+                `
+                }
             });
+
+            const on = document.querySelectorAll('.on');
+            on.forEach(el => {
+                if(el.children[1].innerHTML == destinatario){
+                    el.children[2].classList.remove('escondido');
+                }
+                participanteOnline.push(el.children[1].innerHTML);
+            })
+
+            for(let i = 0; i < participanteOnline.length; i++){
+                if(participanteOnline[i] == destinatario){
+                    destOn = true;
+                }
+                else {
+                    destOn = false;
+                }
+            }
+            if(destOn == false){
+                destinatario = 'Todos';
+            }
+
             numeroParticipantes = data.length;
         }
     })
@@ -122,14 +167,14 @@ function renderizarMSGS(data) {
         else if (element.type == "message") {
             chat.innerHTML += `
                 <section class="msg" >
-                    ${element.time} <span> ${element.from} </span> para <span>${element.to}</span> ${element.text}
+                    ${element.time} <span> ${element.from} </span> para <span>${element.to}</span> : ${element.text}
                 </section>
             `
         }
         else if (usuario.name == element.to || element.from == usuario.name) {
             chat.innerHTML += `
                 <section class="msg reservada" >
-                    ${element.time} <span> ${element.from} </span> reservadamente para <span>${element.to}</span> ${element.text}
+                    ${element.time} <span> ${element.from} </span> reservadamente para <span>${element.to}</span> : ${element.text}
                 </section>
             `
         }
@@ -167,5 +212,5 @@ form.addEventListener('submit', (e) => {
 buscarMSG();
 buscarParticipantes();
 setInterval(buscarMSG, 2000);
-setInterval(buscarParticipantes, 1000);
+setInterval(buscarParticipantes, 10000);
 
